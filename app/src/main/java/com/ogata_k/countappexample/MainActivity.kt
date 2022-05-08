2f68e4f479014b5652e902b10407dd8dcc0aec61
ogata-k <ogtkzk712@gmail.com>
2022-05-08 18:55:03 +0900

    DataBindingと違い、単方向バインディングのViewBindingを使えるように修正

--- app/src/main/java/com/ogata_k/countappexample/MainActivity.kt
+++ app/src/main/java/com/ogata_k/countappexample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ogata_k.countappexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import com.ogata_k.countappexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // cf. https://developer.android.com/topic/libraries/view-binding?hl=ja
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // @todo set binding value e.g. binding.initialText = <livedata>
     }
 }
