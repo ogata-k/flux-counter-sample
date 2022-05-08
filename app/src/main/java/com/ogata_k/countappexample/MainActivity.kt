04ec4cb042c25ac9dd5ed12e111b2d9ccf4863a0
ogata-k <ogtkzk712@gmail.com>
2022-05-08 18:15:19 +0900

    initial commit

---
+++ app/src/main/java/com/ogata_k/countappexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ogata_k.countappexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
